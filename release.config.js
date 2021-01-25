2ab0abce54c818a59fafc9c4b584b4275be53268
Ben Limmer <ben.limmer@ibotta.com>
2021-01-25 11:39:12 -0700

    basic semantic-release config

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: [
+    "release",
+    { name: "preview", channel: "alpha", prerelease: "alpha" },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        // We're not publishing a package to `npm` or artifactory, so disable the
+        // publish step. This plugin still updates the version in package.json.
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
